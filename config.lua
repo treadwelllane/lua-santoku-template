724a9f56391d1c36c8899de26f2560585ff15d41
broma0 <121064585+broma0@users.noreply.github.com>
2023-12-12 21:12:02 +0000

    update config.lua

--- config.lua
+++ config.lua
@@ -1,18 +1,19 @@
 local env = {
 
   name = "santoku-template",
-  version = "0.0.1-1",
+  version = "0.0.2-1",
   variable_prefix = "TK_TEMPLATE",
   license = "MIT",
   public = true,
 
   dependencies = {
     "lua >= 5.1",
-    "santoku >= 0.0.143-1",
-    "santoku-fs >= 0.0.1-1"
+    "santoku >= 0.0.145-1",
+    "santoku-fs >= 0.0.2-1"
   },
 
   test_dependencies = {
+    "santoku-test >= 0.0.1-1",
     "luassert >= 1.9.0-1",
     "luacheck >= 1.1.0-1",
     "luacov >= 0.15.0-1",
