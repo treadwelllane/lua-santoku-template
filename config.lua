8ad6649ebca9ad4927105d0b03d8658072245781
broma0 <121064585+broma0@users.noreply.github.com>
2023-12-20 23:01:22 +0000

    update config.lua template.lua todo.md

--- config.lua
+++ config.lua
@@ -1,22 +1,22 @@
 local env = {
 
   name = "santoku-template",
-  version = "0.0.4-1",
+  version = "0.0.5-1",
   variable_prefix = "TK_TEMPLATE",
   license = "MIT",
   public = true,
 
   dependencies = {
     "lua >= 5.1",
-    "santoku >= 0.0.148-1",
-    "santoku-fs >= 0.0.6-1"
+    "santoku >= 0.0.151-1",
+    "santoku-fs >= 0.0.7-1"
   },
 
   test_dependencies = {
     "santoku-test >= 0.0.4-1",
     "luassert >= 1.9.0-1",
-    "luacheck >= 1.1.0-1",
     "luacov >= 0.15.0-1",
+    "inspect >= 3.1.3-0"
   },
 
 }
