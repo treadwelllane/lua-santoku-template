1ed30d92669bc3c4a421bc437ad5c4b040796c1a
broma0 <121064585+broma0@users.noreply.github.com>
2026-04-18 13:45:04 -0400

    bump to 0.0.38-1, update license and deps

--- make.lua
+++ make.lua
@@ -1,14 +1,14 @@
 local env = {
   name = "santoku-template",
-  version = "0.0.37-1",
+  version = "0.0.38-1",
   variable_prefix = "TK_TEMPLATE",
   license = "MIT",
   public = true,
   dependencies = {
     "lua == 5.1",
     "lpeg >= 1.1.0-2",
-    "santoku >= 0.0.324-1",
-    "santoku-fs >= 0.0.43-1"
+    "santoku >= 0.0.328-1",
+    "santoku-fs >= 0.0.45-1"
   },
 }
 
