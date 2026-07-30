2004f91319a79ce73a3f7a06b64ab6926e63bc52
broma0 <121064585+broma0@users.noreply.github.com>
2026-07-30 19:01:30 -0400

    bump version to 1.0.0-1

--- make.lua
+++ make.lua
@@ -1,14 +1,14 @@
 local env = {
   name = "santoku-template",
-  version = "0.0.40-1",
+  version = "1.0.0-1",
   variable_prefix = "TK_TEMPLATE",
   license = "MIT",
   public = true,
   dependencies = {
     "lua == 5.1",
-    "santoku-lpeg >= 0.0.8-1",
-    "santoku >= 0.0.328-1",
-    "santoku-fs >= 0.0.45-1"
+    "santoku-lpeg >= 1.0.0, < 2.0.0",
+    "santoku >= 1.0.0, < 2.0.0",
+    "santoku-fs >= 1.0.0, < 2.0.0"
   },
 }
 
